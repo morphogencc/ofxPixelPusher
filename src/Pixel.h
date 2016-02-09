7bd4c044a0971b0cf3bdab5e208096ae0a02ecff
nathan lachenmyer <nathan@smalldesignfirm.com>
2016-02-09 12:50:08 -0500

    fixed misplaced comments in Pixel.h

--- src/Pixel.h
+++ src/Pixel.h
@@ -7,31 +7,31 @@
 
 #pragma once
 
-//forward declare Strip so I don't get a Pixel<->Strip interdependency
 namespace ofxPixelPusher {
 
-class Strip;
+	//forward declare Strip so I don't get a Pixel<->Strip interdependency
+	class Strip; 
 
-class Pixel {
-  friend class Strip;
- public:
-  Pixel();
-  Pixel(unsigned char r, unsigned char g, unsigned char b);
-  Pixel(unsigned char r, unsigned char g, unsigned char b, unsigned char o, unsigned char w);
-  ~Pixel();
-  void setColor(unsigned char r, unsigned char g, unsigned char b);
-  void setColor(unsigned char r, unsigned char g, unsigned char b, unsigned char o, unsigned char w);
-  void setColor (Pixel pixel);
-  void setAntiLog(bool useAntiLog);
- protected:
-  bool mUseAntiLog;
-  unsigned char mRed;
-  unsigned char mGreen;
-  unsigned char mBlue;
-  unsigned char mOrange;
-  unsigned char mWhite;
-  static unsigned char mLinearExp[256];
-  
-};
+	class Pixel {
+		friend class Strip;
+	public:
+		Pixel();
+		Pixel(unsigned char r, unsigned char g, unsigned char b);
+		Pixel(unsigned char r, unsigned char g, unsigned char b, unsigned char o, unsigned char w);
+		~Pixel();
+		void setColor(unsigned char r, unsigned char g, unsigned char b);
+		void setColor(unsigned char r, unsigned char g, unsigned char b, unsigned char o, unsigned char w);
+		void setColor(Pixel pixel);
+		void setAntiLog(bool useAntiLog);
+	protected:
+		bool mUseAntiLog;
+		unsigned char mRed;
+		unsigned char mGreen;
+		unsigned char mBlue;
+		unsigned char mOrange;
+		unsigned char mWhite;
+		static unsigned char mLinearExp[256];
+
+	};
 
 }
